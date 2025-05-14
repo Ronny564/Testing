2559723794998f9a18e07db5b720aec424087d23
Johnny <115782987+ronny564@users.noreply.github.com>
2025-05-14 11:30:48 +0630

    First Commit

---
+++ src/main/java/com/db_test/Main.java
@@ -0,0 +1,15 @@
+package com.db_test;
+
+/**
+    This is testing Main Class
+    Created by Lin MyaT Thu
+    Created data 13/5/2025
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        // This is
+        System.out.println("Hello and welcome!");
+    }
+
+}
